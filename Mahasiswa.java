1b5d5429b1f4ba0bc2ea77fc0f0fb463d1926fda
Yun27 <81316506+Yun27@users.noreply.github.com>
2021-06-01 17:15:02 +0800

    Menambahkan Mahasiswa.java ke branch master

---
+++ Mahasiswa.java
@@ -0,0 +1,49 @@
+public class Mahasiswa{
+	private String nim;
+	private String nama;
+	private String alamat;
+	private String prodi;
+
+	public Mahasiswa(){
+
+	}
+
+	public Mahasiswa(String nim, String nama, String alamat, String prodi){
+		this.nim = nim;
+		this.nama = nama;
+		this.alamat = alamat;
+		this.prodi = prodi;
+	}
+
+	public void setNim(String nim){
+		this.nim = nim;
+	}
+
+	public String getNim(){
+		return this.nim;
+	}
+
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+
+	public String getNama(){
+		return this.nama;
+	}
+
+	public void setAlamat(String alamat){
+		this.alamat = alamat;
+	}
+
+	public String getAlamat(){
+		return this.alamat;
+	}
+
+	public void setProdi(String prodi){
+		this.prodi = prodi;
+	}
+
+	public String getProdi(){
+		return this.prodi;
+	}
+}
